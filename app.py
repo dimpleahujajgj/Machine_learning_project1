91c4f8391b3cbcc5fe87ae1846efd18feb050318
dimpleahujajgj <145999213+dimpleahujajgj@users.noreply.github.com>
2023-10-02 11:53:11 +0530

    Success

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting Machine learning project"
+    return "CI/CD has been established"
 
 if __name__=="__main__":
     app.run(debug=True) 
